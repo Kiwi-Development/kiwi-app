40d19e1c3a4d19c1c489dd44ae3af9e3e96dd7a8
Skai Nzeuton <san82@cornell.edu>
2025-11-23 00:31:07 -0500

    Fix cookie/auth session redirect issue

---
+++ src/app/provider.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { SessionContextProvider } from "@supabase/auth-helpers-react"
+import { useState } from "react"
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    const [supabaseClient] = useState(() => createPagesBrowserClient())
+
+    return (
+        <SessionContextProvider supabaseClient={supabaseClient}>
+            {children}
+        </SessionContextProvider>
+    )
+}
