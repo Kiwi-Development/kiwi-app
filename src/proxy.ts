8384d8e7ab35bd2c3d2c8367b67cf3f6cd16ad45
Skai Nzeuton <san82@cornell.edu>
2025-11-22 22:50:54 -0500

    Prevent unauthorized users from accessing dashboard pages

---
+++ src/proxy.ts
@@ -0,0 +1,28 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export async function proxy(req: NextRequest) {
+    const res = NextResponse.next()
+    const supabase = createMiddlewareClient({ req, res })
+
+    // Refresh session if expired - required for Server Components
+    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
+    const {
+        data: { user },
+        error,
+    } = await supabase.auth.getUser()
+
+    console.log(user, error);
+
+    // If user is not signed in and the current path is /dashboard, redirect the user to /
+    if ((error || !user) && req.nextUrl.pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/', req.url))
+    }
+
+    return res
+}
+
+export const config = {
+    matcher: ['/dashboard/:path*'],
+}
